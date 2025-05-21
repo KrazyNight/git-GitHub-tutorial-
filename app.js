0cdebb75f53c25886170233befd755d1d2462ef6
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-05-20 17:41:55 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// let counter = 1
+
+// while (counter <= 20) {
+//     console.log('while loop ran');
+//     counter = counter + 1;
+
+
+// }
+// console.log('while loop finished running')
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+for (let i = 1; i <= 15 ;i++) {
+    // console.log('i');
+    // i % 2 !== 0 ? console.log('odd ASAP'): console.log('even Frontend');
+    // i % 5 === 0 ? console.log('divicible by 5,  ASAP Frontend') : console.log('not divivible by 5');
+
+};
+console.log('This is a for loop that loop through 1 to 15');
