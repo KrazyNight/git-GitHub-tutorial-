9c965a1c8955ee559037761da8bdd134014e8544
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-05-20 18:47:21 -0700

    app.js 3

--- app.js
+++ app.js
@@ -12,10 +12,33 @@
 //     console.log(i)
 // }
 
-for (let i = 1; i <= 15 ;i++) {
-     console.log(`${i}`);
-    i % 2 !== 0 ? console.log('odd ASAP'): console.log('even Frontend');
-    i % 5 === 0 ? console.log('divicible by 5,  ASAP Frontend') : console.log('not divivible by 5');
 
-};
-console.log('This is a for loop that loop through 1 to 15');
+// question #!
+
+// for (let i = 1; i <= 15 ;i++) {
+//      console.log(`${i}`);
+//     i % 2 !== 0 ? console.log('odd ASAP'): console.log('even Frontend');
+//     i % 5 === 0 ? console.log('divicible by 5,  ASAP Frontend') : console.log('not divivible by 5');
+
+// };
+// console.log('This is a for loop that loop through 1 to 15');
+
+// for (let i = 1; i <= 15; i++) {
+//     if ( i % 5 === 0) {
+//         console.log(`${i} -- ASAP Frontend`)
+//     }
+//     else if ( i % 2 === 0 ) {
+//         console.log(`${i} -- e Frontend`)
+//     }
+//     else {
+//         console.log(`${i} -- o ASAP`)
+//     }
+
+// };
+
+// Q: print all characters from the string
+const  str = "ASAP Frontend"
+
+for (let i = ) {
+
+}
