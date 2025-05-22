2c04930c7578bf824a893f15a2835200166c8f14
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-05-22 13:41:33 -0700

    app.js 6

--- app.js
+++ app.js
@@ -116,7 +116,7 @@
 
 /**
  * Night Club Exercise
- * 
+ *
  * Filter out all people who are not 18+ yeara old
  */
 // let age = [18, 20, 16, 15, 21]
@@ -125,7 +125,7 @@
 //     if (element >= 18) {
 //         return true;
 //     }
-// }) 
+// })
 
 // console.log(ageFilterArray)
 
@@ -135,23 +135,137 @@
 
 /**
  * Night Club Exercise
- * 
+ *
  * Filter out all people who are not 18+ years old
- * w/out using the .filter method 
+ * w/out using the .filter method
  */
-let age = [18, 20, 16, 15, 21];
-let pass = []
-// loop through array using for loop 
-//simple array of element 
-for (let i = 0; i < age.length; i++) {
-    console.log(age[i])
-    if (age[i] >= 18) {
-        
-        pass.push(age[i])
-    }
-    // check if age is 18+
-    //true, add to new empty array
-    
+// let age = [18, 20, 16, 15, 21];
+// let pass = []
+// // loop through array using for loop
+// //simple array of element
+// for (let i = 0; i < age.length; i++) {
+//     console.log(age[i])
+//     if (age[i] >= 18) {
 
-} 
-console.log(pass)
+//         pass.push(age[i])
+//     }
+//     // check if age is 18+
+//     //true, add to new empty array
+// }
+// console.log(pass)
+
+// let array = [1, 2, 3, 4, 5];
+
+// let newArray = array.map((e) => {
+//   console.log(e);
+//   return null;
+// });
+// console.log(newArray);
+
+/**
+ * Currency converter
+ *
+ * Q1: turn each element in an array from USD to AUD
+ * AUD = USD X 1.5
+ *
+ * Q2: w/out using map method
+ */
+
+//Q1:
+// let usDollars = [10, 20, 30, 40];
+
+// let AUD = usDollars.map((e) => {
+//     console.log(e)
+//     return e * 1.5
+// })
+// console.log(AUD)
+
+//Q2:
+// let usDollars = [10, 20, 30, 40];
+// let AUD = [];
+
+// for (let i = 0; i < usDollars.length; i++) {
+//     console.log(usDollars[i]);
+//     AUD.push(usDollars[i] * 1.5);
+// };
+// console.log(AUD)
+
+/**
+ *
+ * Objects ---- think of dic/ dictionaries '{}'
+ *
+ */
+
+// let userEmail = "erik@gmail.com";
+// let userPassword = 'Thereyn0';
+// let userName = "erik"
+// let userDiscord = "exampleDiscord"
+// let userSubscribtion = "VIP+"
+
+// let users = [
+//   {
+//     Email: "erik@gmail.com",
+//     Password: "Thereyn0",
+//     Name: "erik",
+//     Discord: "exampleDiscord",
+//     Subscribtion: "VIP+",
+//     lessonsCompleted: [1, 2, 3],
+//   },
+//   {
+//     Email: "jack@gmail.com",
+//     Password: "Thereyn0!",
+//     Name: "jack",
+//     Discord: "jexampleDiscord",
+//     Subscribtion: "VIP+",
+//     lessonsCompleted: [1, 2, 3, 4, 5],
+//   },
+// ];
+// console.log(users);
+//console.log(users[0].Email)
+// ex. increase lessonsComplete by 3
+//console.log(users[0].lessonsCompleted.map((e) => e * 3))
+
+/**
+ * Create a function that accepts the parameters
+ * -email
+ * -password
+ * -name
+ * -discord
+ * -subscription
+ * -lessonsCompleted
+ * 
+ * Inside the Sign Up funtion:
+ * -use parameters to create a user object
+ * -push that object onto the 'users' array.
+ */
+// function signUp(email, password, name, discord, subscription, lessonsCompleted) {
+//     return {
+//         email: 'signUp',
+//         password: 'signUp',
+//         name: 'signUp',
+//         discord: 'signUp',
+//         subscription: 'signUp',
+//         lessonsCompleted: 'signUp',
+//     }
+// }
+
+// console.log(signUp)
+
+// let myObject = signUp();
+// console.log(myObject)
+// //users.push(myObject)
+// console.log(users.push(myObject))
+
+// function signUp(user) {
+//     users.push(user)
+// }
+// signUp({
+//         email: "email@gmail.com",
+//         password: "Thereyn0",
+//         name: "erik",
+//         discord: "ediscord",
+//         subscription: "v++",
+//         lessonsCompleted: [1, 2]
+//     })
+
+// console.log(users)
