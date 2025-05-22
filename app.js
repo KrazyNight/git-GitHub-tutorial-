dc3435ede0980c9fcb342485d279259b0037a3e8
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-05-21 18:13:55 -0700

    app.js 5

--- app.js
+++ app.js
@@ -3,16 +3,12 @@
 // while (counter <= 20) {
 //     console.log('while loop ran');
 //     counter = counter + 1;
-
-
 // }
 // console.log('while loop finished running')
 
 // for (let i = 0; i < 5; i++) {
 //     console.log(i)
 // }
-
-
 // question #!
 
 // for (let i = 1; i <= 15 ;i++) {
@@ -37,8 +33,125 @@
 // };
 
 // Q: print all characters from the string
-const  str = "ASAP Frontend"
+// let str = "ASAP Frontend";
 
-for (let i = ) {
+// for (let i = 0; i <= str.length; i++) {
+//     console.log(str[i]);
+// }
 
-}
+// Pass
+// Function Declaration
+
+// function greetuser(name, time) {
+//     console.log(`Greetings ${time}, ${name}`);
+// }
+
+// // call function
+// greetuser('erik', 'morning');
+// greetuser('alex', 'evening');
+
+// function main() {
+//     return 10;
+//     console.log('hello world');
+// }
+
+// console.log(main())
+
+/** Create a Function that converts $USD into $AUD
+ *
+ * $AUD to $USD Formula:
+ * AUD = USD X 1.5
+ */
+
+// Regular Function
+// function covertUsdToAud(usd) {
+//     return usd * 1.5;
+
+// }
+
+// console.log(covertUsdToAud(1000));
+// console.log(covertUsdToAud(1000));
+
+// Arrow Functiom
+
+// const covertUsdToAud = (usd) => {
+//   return usd * 1.5;
+// };
+
+// console.log(covertUsdToAud(1000))
+
+// let array = ["Apples", "Bananas", "Eggs", "Milk"]
+// // First Element in array
+// console.log(array[0])
+// // Last Element of array
+// console.log(array[array.length - 1])
+
+// array.push("Bread")
+
+// console.log(array)
+
+//let array = [20, 15, 10, 20]
+// array methods
+
+//array.filter(() => {});
+// long way
+// let filterArray = array.filter((element) => {
+//     console.log(element);
+//     if (element <= 15) {
+//         return true;
+//     }
+// })
+// console.log(filterArray);
+
+// let filterArray = array.filter((element) => {
+//     return element <= 15;
+// });
+
+// console.log(filterArray)
+
+// even shorter
+// let filterArray = array.filter((element) => element <= 15);
+
+// console.log(filterArray)
+
+/**
+ * Night Club Exercise
+ * 
+ * Filter out all people who are not 18+ yeara old
+ */
+// let age = [18, 20, 16, 15, 21]
+
+// let ageFilterArray = age.filter((element) => {
+//     if (element >= 18) {
+//         return true;
+//     }
+// }) 
+
+// console.log(ageFilterArray)
+
+// let ageFilterArray = age.filter((e) => e >= 18);
+
+// console.log(ageFilterArray)
+
+/**
+ * Night Club Exercise
+ * 
+ * Filter out all people who are not 18+ years old
+ * w/out using the .filter method 
+ */
+let age = [18, 20, 16, 15, 21];
+let pass = []
+// loop through array using for loop 
+//simple array of element 
+for (let i = 0; i < age.length; i++) {
+    console.log(age[i])
+    if (age[i] >= 18) {
+        
+        pass.push(age[i])
+    }
+    // check if age is 18+
+    //true, add to new empty array
+    
+
+} 
+console.log(pass)
